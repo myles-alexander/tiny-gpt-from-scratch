6a846b75d5810e4cd18f805976fa4a5f9926f6ca
myles-alexander <52891158+myles-alexander@users.noreply.github.com>
2026-09-06 22:54:49 +0100

    Step 2: build_stoi

--- model.py
+++ model.py
@@ -12,8 +12,12 @@ def build_vocab(text):
     # TODO: return a sorted list of every unique character in text
     return sorted(set(text))
 
-# Step 2 - build_stoi (not yet solved)
-# TODO: implement
+# Step 2 - build_stoi
+def build_stoi(vocab):
+    """Return a dict mapping each character in vocab to its index."""
+    # TODO: map each character in vocab to its integer position
+    to_index = {v:i for i,v in enumerate(vocab)}
+    return to_index
 
 # Step 3 - build_itos (not yet solved)
 # TODO: implement
