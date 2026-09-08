7180c8460d91e803c3322a3798e5723903b88687
myles-alexander <52891158+myles-alexander@users.noreply.github.com>
2026-09-08 22:38:56 +0100

    Step 31: softmax_overflow_demo

--- model.py
+++ model.py
@@ -230,8 +230,16 @@ def naive_softmax_1d(logits):
     # TODO: exponentiate the logits, then divide by their total sum
     return np.exp(logits) / np.sum(np.exp(logits))
 
-# Step 31 - softmax_overflow_demo (not yet solved)
-# TODO: implement
+# Step 31 - softmax_overflow_demo
+def softmax_overflow_demo(large_value):
+    """Show that naive exp overflows on a large logit.
+
+    Return {'naive_exp': float, 'overflowed': bool}.
+    """
+    # TODO: exponentiate large_value via array_exp and report whether it is inf.
+    exp = array_exp(large_value)
+    overflowed = True if exp == np.inf else False
+    return {'naive_exp':exp, 'overflowed': overflowed}
 
 # Step 32 - stable_softmax_1d (not yet solved)
 # TODO: implement
