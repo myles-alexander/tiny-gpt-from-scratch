e56105de68e603ad3bd4d6cb108eb5c1cc5d4a7e
myles-alexander <52891158+myles-alexander@users.noreply.github.com>
2026-09-17 22:20:11 +0100

    Step 56: average_nll

--- model.py
+++ model.py
@@ -417,8 +417,10 @@ def sum_negative_log_probs(p_matrix, data):
     # TODO: sum the negative log probabilities of all consecutive bigrams in data
     return -sum(log_prob_of_pair(p_matrix,data[d],data[d+1]) for d in range(len(data)-1))
 
-# Step 56 - average_nll (not yet solved)
-# TODO: implement
+# Step 56 - average_nll
+def average_nll(p_matrix, data):
+    # TODO: return mean negative log likelihood per bigram over consecutive pairs in data.
+    return sum_negative_log_probs(p_matrix,data) / (len(data)-1)
 
 # Step 57 - initialize_w_random (not yet solved)
 # TODO: implement
