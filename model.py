e3de5ab5875a8222f0776bb7784c4d5fec41a699
myles-alexander <52891158+myles-alexander@users.noreply.github.com>
2026-09-17 22:16:54 +0100

    Step 55: sum_negative_log_probs

--- model.py
+++ model.py
@@ -412,8 +412,10 @@ def log_prob_of_pair(p_matrix, current_id, next_id):
     # TODO: pick out P[current_id, next_id] and return its natural log
     return array_log(index_element(p_matrix,current_id,next_id))
 
-# Step 55 - sum_negative_log_probs (not yet solved)
-# TODO: implement
+# Step 55 - sum_negative_log_probs
+def sum_negative_log_probs(p_matrix, data):
+    # TODO: sum the negative log probabilities of all consecutive bigrams in data
+    return -sum(log_prob_of_pair(p_matrix,data[d],data[d+1]) for d in range(len(data)-1))
 
 # Step 56 - average_nll (not yet solved)
 # TODO: implement
