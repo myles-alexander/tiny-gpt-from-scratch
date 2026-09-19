87124af110af047a3af6033891269bc5cfa191a4
myles-alexander <52891158+myles-alexander@users.noreply.github.com>
2026-09-19 12:43:32 +0100

    Step 63: logits_to_probs_rowwise

--- model.py
+++ model.py
@@ -463,8 +463,10 @@ def forward_logits_lookup(w, ids):
     # TODO: return the logits for a batch of token ids by direct row lookup into W.
     return w[ids]
 
-# Step 63 - logits_to_probs_rowwise (not yet solved)
-# TODO: implement
+# Step 63 - logits_to_probs_rowwise
+def logits_to_probs_rowwise(logits):
+    # TODO: convert a (B, V) logits matrix into a row-wise probability matrix
+    return stable_softmax_2d_rowwise(logits)
 
 # Step 64 - gather_correct_token_probs (not yet solved)
 # TODO: implement
