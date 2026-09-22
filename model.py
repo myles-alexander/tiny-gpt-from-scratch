a32d750da99e71d0f2bac7f96ebcc8db82fd34a4
myles-alexander <52891158+myles-alexander@users.noreply.github.com>
2026-09-22 15:10:40 +0100

    Step 78: linear_backward_dw

--- model.py
+++ model.py
@@ -599,8 +599,11 @@ def linear_backward_dx(dy, cache):
     # TODO: compute the gradient of the loss w.r.t. the linear layer input X given dy and cache
     return dy @ cache['w'].T
 
-# Step 78 - linear_backward_dw (not yet solved)
-# TODO: implement
+# Step 78 - linear_backward_dw
+def linear_backward_dw(dy, cache):
+    """Return dL/dW for a linear layer Y = X @ W."""
+    # TODO: compute the weight gradient using x from cache and the upstream dy
+    return cache['x'].T @ dy
 
 # Step 79 - bias_add_forward (not yet solved)
 # TODO: implement
